9ee0281494e4dc5970f2abbf550e1768790aecbd
Pierre-Alexandre RACINE <pierrealexandreracine@gmail.com>
2013-06-20 17:16:21 +0200

    Valeurs par défaut pour les champs de date dans l'interface pour poster les kuchikomi.

--- web/vue_interface.php
+++ web/vue_interface.php
@@ -1,6 +1,8 @@
 <?php
 include_once('../includes/entete_marc.php');
 
+$now = date("Y-m-d");
+
 echo '<section>
 	<p><br /><a class="btn btn-medium btn-info margin:auto;" href="espmarc.php"><i class="icon-white icon-chevron-left"></i> Retour</a></p>
       <form action="espmarc.php?appel=interface" method="post" enctype="multipart/form-data">
@@ -11,9 +13,9 @@ echo '<section>
       
       <p><label for="photokk">Ajouter une photo (2Mo maximum) :<br /> <input type="file" name="photokk" id="photokk" /></label></p>
       
-      <p><label for="date_debut">Date de début :<br /> <input type="date" name="date_debut" id="date_debut" class="input-medium" /></label></p>
+      <p><label for="date_debut">Date de début :<br /> <input type="date" name="date_debut" id="date_debut" value="' . $now . 	'" class="input-medium" /></label></p>
       
-      <p><label for="date_fin">Date de fin :<br />  <input type="date" name="date_fin" id="date_fin" class="input-medium" /></label></p>
+      <p><label for="date_fin">Date de fin :<br />  <input type="date" name="date_fin" id="date_fin" value="' . $now . 	'" class="input-medium" /></label></p>
       
       
       <textarea name ="mentions" id="mentions" rows="5">Conditions particulières, mentions légales, etc...</textarea>
