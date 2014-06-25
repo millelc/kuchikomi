8508e39d9219c9d73e1a8c8c46fce3c93fed81d2
Nicolas Dries <nicolas.dries@nearforge.com>
2014-06-25 10:55:00 +0200

    Ajout des dataFixtures pour KuchiAccount et SubscriptionGroup

---
+++ src/obdo/KuchiKomiRESTBundle/DataFixtures/ORM/KuchiAccountData.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace obdo\KuchiKomiRESTBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+use obdo\KuchiKomiRESTBundle\Entity\KuchiAccount;
+
+use Symfony\Component\Security\Acl\Domain\ObjectIdentity;
+use Symfony\Component\Security\Acl\Domain\UserSecurityIdentity;
+use Symfony\Component\Security\Acl\Permission\MaskBuilder;
+
+class KuchiAccountData extends AbstractFixture implements ContainerAwareInterface, OrderedFixtureInterface
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getOrder()
+    {
+            return 3;
+    }
+
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    // Dans l'argument de la méthode load, l'objet $manager est l'EntityManager
+    public function load(ObjectManager $manager)
+    {
+        $manager->getConnection()->exec("ALTER TABLE KuchiAccount AUTO_INCREMENT = 1;");
+
+
+
+    }
+
+}
---
+++ src/obdo/KuchiKomiRESTBundle/DataFixtures/ORM/SubscriptionGroupData.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace obdo\KuchiKomiRESTBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+use obdo\KuchiKomiRESTBundle\Entity\SubscriptionGroup;
+
+class SubscriptionGroupData extends AbstractFixture implements ContainerAwareInterface, OrderedFixtureInterface
+{
+	/**
+	 * @var ContainerInterface
+	 */
+	private $container;
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	public function getOrder()
+	{
+		return 4;
+	}
+	
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	public function setContainer(ContainerInterface $container = null)
+	{
+		$this->container = $container;
+	}
+	
+	// Dans l'argument de la méthode load, l'objet $manager est l'EntityManager
+	public function load(ObjectManager $manager)
+	{
+		
+		$manager->getConnection()->exec("ALTER TABLE SubscriptionGroup AUTO_INCREMENT = 1;");
+		
+		$manager->flush();	
+	}
+}
