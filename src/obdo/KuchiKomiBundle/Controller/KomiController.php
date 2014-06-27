5f28b9788827d82b82bc9c7a2b048bf6849aadc9
Nicolas Dries <nicolas.dries@nearforge.com>
2014-06-27 21:25:33 +0200

    Supprime la possibilité d'envoyer une notification depuis l'interface web komiIndex

--- src/obdo/KuchiKomiBundle/Controller/KomiController.php
+++ src/obdo/KuchiKomiBundle/Controller/KomiController.php
@@ -22,34 +22,4 @@ class KomiController extends Controller
                                                                                         'sort'   => $sort
                                                                                         ));
     }
-    
-    public function notificationAction( $id, $page, $sort )
-    {
-    	$em = $this->getDoctrine()->getManager();
-    	$repositoryKomi = $em->getRepository('obdoKuchiKomiRESTBundle:Komi');
-    	$komi = $repositoryKomi->findOneById($id);
-    	// Post message
-    	if( $komi->getOsType() == 0 )
-    	{
-    		$date = new \DateTime('now', new \DateTimeZone('Europe/Paris'));
-    		$dateString = $date->format('Y-m-d H:i:s');
-    		
-    		$message = new AndroidMessage();
-    		$message->setGCM(true);
-    		$message->setMessage('Nouveau KuchiKomi (' . $dateString . ')');
-    		$message->setData(array("type" => "1"));
-    		$message->setDeviceIdentifier($komi->getGcmRegId());
-    	}
-    	
-    	$this->container->get('rms_push_notifications')->send($message);
-    	
-    	$komis = $repositoryKomi->getKomis(25, $page, $sort);
-    	
-    	return $this->render('obdoKuchiKomiBundle:Default:komiindex.html.twig', array(
-    			'komis'   => $komis,
-    			'page'   => $page,
-    			'nombrePage' => ceil(count($komis)/25),
-    			'sort'   => $sort
-    	));
-    }
 }
